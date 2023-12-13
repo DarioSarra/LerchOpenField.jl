aae4363e02a8b2776910ae6182895dd7b7dab833
Documenter.jl <documenter@juliadocs.github.io>
2023-12-13 14:53:49 +0000

    build based on a43f8b4

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = LerchOpenField","category":"page"},{"location":"#LerchOpenField","page":"Home","title":"LerchOpenField","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for LerchOpenField.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [LerchOpenField]","category":"page"}]
+}
